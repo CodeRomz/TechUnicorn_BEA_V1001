c30c94e2e025fefcb5abc613ab8267ac9529387f
CodeRomz <romualdojrb@yahoo.com>
2022-11-15 20:45:24 +0400

    Coding

--- TestCode.py
+++ TestCode.py
@@ -2,5 +2,10 @@ from TubeaDatabaseConnection import TubeaDbExec
 
 view_appointment = TubeaDbExec("appointment")
 
+datalist = []
 for data in view_appointment.appointment_data_checker(1, 'doctor'):
-    print(data)
+    # print(data)
+    datalist.append(data)
+
+
+print(datalist)
